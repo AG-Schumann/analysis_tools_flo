1e2f3d1c770dcad23e0bd490a7ff3cfa4d28f7ad
Florian <florian.toennies@physik.uni-freiburg.de>
2022-10-28 15:08:34 +0200

    silencer resets stdout even in case f causes an exception

--- flo_decorators.py
+++ flo_decorators.py
@@ -2,18 +2,21 @@ import sys
 
 def silencer(f):
     '''
-    decorator that adds argument "silent" to function that temporarily sets the stdout to dev/null
-    
+    decorator that adds argument "silent" to function:
+    if set to True: sets the stdout to dev/null during call    
     '''
 
     def wrapper(*args, silent = False, **kwargs):
-        
+        ret = None
         sys_stdout = sys.stdout
         if silent is True: 
             sys.stdout = open('/dev/null', 'w')
-        
-        ret = f(*args, **kwargs)
-        sys.stdout = sys_stdout
-        return(ret)
+        try:
+            ret = f(*args, **kwargs)
+        finally:
+            # just make sure the stdout is reset
+            pass
+            sys.stdout = sys_stdout
+            return(ret)
         
     return wrapper
